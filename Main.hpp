2323618bc52105ec64880bbfa70fb97d00465014
GUINET virgile <virgile.guinet@gmail.com>
2022-12-20 15:51:29 +0100

    Update Main.hpp, Main.cpp files

---
+++ Main.hpp
@@ -0,0 +1,15 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+#include "DominoCarre.hpp"
+#include "Trax/Trax.hpp"
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+class Main{
+    public :
+    static void openMenuPrincipal();
+};
+
+#endif
